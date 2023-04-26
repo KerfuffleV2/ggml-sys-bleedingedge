802fa9c71300333dba71df662082bfce091f1d72
KerfuffleV2 <kerfliffle@keemail.me>
2023-04-26 10:40:17 -0600

    Clean up build.rs

--- build.rs
+++ build.rs
@@ -1,42 +1,14 @@
 // Build script and bindings generation modified from https://github.com/rustformers/llama-rs
 
-use std::{env, fs::metadata, path::PathBuf};
+use std::{collections::HashSet, env, path::PathBuf};
+
+const GGML_SOURCE_DIR: &str = "ggml-src";
+const GGML_HEADER: &str = "ggml.h";
+const GGML_SOURCE: &str = "ggml.c";
 
 fn generate_bindings() {
-    const SRC_FILES: &[&str] = &["ggml.h", "ggml.c"];
-
-    let ggml_paths = SRC_FILES
-        .iter()
-        .map(|fname| PathBuf::from("ggml-src").join(fname))
-        .collect::<Vec<_>>();
-    let ggml_header_path = &ggml_paths[0];
-    let ggml_mdata = ggml_paths
-        .iter()
-        .map(|p| metadata(p).expect(&format!("Cannot get metadata for {p:?}")))
-        .collect::<Vec<_>>();
+    let ggml_header_path = PathBuf::from(GGML_SOURCE_DIR).join(GGML_HEADER);
     let librs_path = PathBuf::from("src").join("lib.rs");
-    if let Ok(lib_md) = metadata(&librs_path) {
-        assert!(
-            ggml_mdata.iter().all(|md| md.is_file()),
-            "Unexpected non-file in source list"
-        );
-        assert!(lib_md.is_file(), "lib.rs unexpectedly isn't a file?");
-        let ggml_stamps = ggml_mdata
-            .iter()
-            .map(|md| {
-                md.modified()
-                    .or_else(|_| md.created())
-                    .expect("Could not get timestamp")
-            })
-            .collect::<Vec<_>>();
-        let lib_stamp = lib_md
-            .modified()
-            .or_else(|_| lib_md.created())
-            .expect("Couldn't get timestamp for lib.rs");
-        if lib_md.len() > 0 && ggml_stamps.iter().all(|stamp| &lib_stamp >= stamp) {
-            return;
-        }
-    }
 
     let bindings = bindgen::Builder::default()
         .header(ggml_header_path.to_string_lossy())
@@ -62,11 +34,10 @@ fn main() {
     // and you will need to manually specify target features through target-features.
 
     println!("cargo:rerun-if-changed=ggml-src");
-    println!("cargo:rerun-if-changed=src/lib.rs");
 
     generate_bindings();
 
-    let ggml_source_path = PathBuf::from("ggml-src").join("ggml.c");
+    let ggml_source_path = PathBuf::from(GGML_SOURCE_DIR).join(GGML_SOURCE);
     let mut builder = cc::Build::new();
     let build = builder.files([ggml_source_path].iter()).include("include");
 
@@ -84,30 +55,14 @@ fn main() {
             if compiler.is_like_clang() || compiler.is_like_gnu() {
                 build.flag("-pthread");
 
-                if features.avx {
-                    build.flag("-mavx");
-                }
-                if features.avx2 {
-                    build.flag("-mavx2");
-                }
-                if features.fma {
-                    build.flag("-mfma");
-                }
-                if features.f16c {
-                    build.flag("-mf16c");
-                }
-                if features.sse3 {
-                    build.flag("-msse3");
-                }
+                features.iter().for_each(|feat| {
+                    build.flag(&format!("-m{feat}"));
+                });
             } else if compiler.is_like_msvc() {
-                match (features.avx2, features.avx) {
-                    (true, _) => {
-                        build.flag("/arch:AVX2");
-                    }
-                    (_, true) => {
-                        build.flag("/arch:AVX");
-                    }
-                    _ => {}
+                if features.contains("avx2") {
+                    build.flag("/arch:AVX2");
+                } else if features.contains("avx") {
+                    build.flag("/arch:AVX");
                 }
             }
         }
@@ -115,86 +70,75 @@ fn main() {
             if compiler.is_like_clang() || compiler.is_like_gnu() {
                 if std::env::var("HOST") == std::env::var("TARGET") {
                     build.flag("-mcpu=native");
-                } else {
-                    #[allow(clippy::single_match)]
-                    match target_os.as_str() {
-                        "macos" => {
-                            build.flag("-mcpu=apple-m1");
-                            build.flag("-mfpu=neon");
-                        }
-                        _ => {}
-                    }
+                } else if &target_os == "macos" {
+                    build.flag("-mcpu=apple-m1");
+                    build.flag("-mfpu=neon");
                 }
                 build.flag("-pthread");
             }
         }
-        _ => {}
+        _ => (),
     }
 
-    #[allow(clippy::single_match)]
-    match target_os.as_str() {
-        "macos" => {
-            build.define("GGML_USE_ACCELERATE", None);
-            println!("cargo:rustc-link-lib=framework=Accelerate");
-        }
-        _ => {}
+    if &target_os == "macos" {
+        build.define("GGML_USE_ACCELERATE", None);
+        println!("cargo:rustc-link-lib=framework=Accelerate");
     }
 
     if is_release {
         build.define("NDEBUG", None);
     }
     build.warnings(false);
-    build.compile("ggml-src");
+    build.compile(GGML_SOURCE_DIR);
 }
 
-fn get_supported_target_features() -> std::collections::HashSet<String> {
+fn get_supported_target_features() -> HashSet<String> {
     env::var("CARGO_CFG_TARGET_FEATURE")
         .unwrap()
         .split(',')
+        .filter(|s| x86::RELEVANT_FLAGS.contains(s))
         .map(ToString::to_string)
-        .collect()
+        .collect::<HashSet<_>>()
 }
 
 mod x86 {
-    #[allow(clippy::struct_excessive_bools)]
-    #[derive(Clone, Debug, PartialEq, Eq)]
-    pub struct Features {
-        pub fma: bool,
-        pub avx: bool,
-        pub avx2: bool,
-        pub f16c: bool,
-        pub sse3: bool,
+    use super::HashSet;
+
+    pub const RELEVANT_FLAGS: &[&str] = &["fma", "avx", "avx2", "f16c", "sse3"];
+    pub struct Features(HashSet<String>);
+
+    impl std::ops::Deref for Features {
+        type Target = HashSet<String>;
+        fn deref(&self) -> &Self::Target {
+            &self.0
+        }
     }
+
     impl Features {
         pub fn get() -> Self {
             #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
             if std::env::var("HOST") == std::env::var("TARGET") {
                 return Self::get_host();
             }
-
-            Self::get_target()
+            Self(super::get_supported_target_features())
         }
 
         #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
         pub fn get_host() -> Self {
-            Self {
-                fma: std::is_x86_feature_detected!("fma"),
-                avx: std::is_x86_feature_detected!("avx"),
-                avx2: std::is_x86_feature_detected!("avx2"),
-                f16c: std::is_x86_feature_detected!("f16c"),
-                sse3: std::is_x86_feature_detected!("sse3"),
-            }
-        }
-
-        pub fn get_target() -> Self {
-            let features = crate::get_supported_target_features();
-            Self {
-                fma: features.contains("fma"),
-                avx: features.contains("avx"),
-                avx2: features.contains("avx2"),
-                f16c: features.contains("f16c"),
-                sse3: features.contains("sse3"),
-            }
+            Self(
+                [
+                    std::is_x86_feature_detected!("fma"),
+                    std::is_x86_feature_detected!("avx"),
+                    std::is_x86_feature_detected!("avx2"),
+                    std::is_x86_feature_detected!("f16c"),
+                    std::is_x86_feature_detected!("sse3"),
+                ]
+                .into_iter()
+                .enumerate()
+                .filter(|(_, exists)| *exists)
+                .map(|(idx, _)| RELEVANT_FLAGS[idx].to_string())
+                .collect::<HashSet<_>>(),
+            )
         }
     }
 }
